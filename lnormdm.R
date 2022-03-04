17620aec766bddf615d5307f6a73ea8725afac45
nraytselis <nraytse@emory.edu>
2022-03-04 16:56:10 -0500

    lnormdm

--- lnormdm.R
+++ lnormdm.R
@@ -13,65 +13,68 @@
 #'
 #' @return ggplot,data
 #' @export
+#' @importFrom rlang .data
 #'
-#' @examples lnormdm(5, names = c("Control", "Treatment 1", "Treatment 2", "Treatment 3", "Treatment 4"), n= c(30,50,50,50,50), meanlog = c(3,4,3,2,3), sdlog= c(1,1,1,1,1))
+#' @examples
+#' lnormdm(3,names=c("Ctrl","Treat1","Treat2"),n=c(30,50,50),meanlog=c(3,4,3),sdlog=c(1,1,1))
 #'
 #'
 #'
-lnormdm <- function(k, names=c(...), n= c(...), meanlog= c(...), sdlog= c(...)){
+lnormdm <- function(k, names, n, meanlog, sdlog){
 
 
-if(k != (length(n)
-         +length(meanlog)
-         +length(sdlog))/3)
-{
+  if(k != (length(n)
+           +length(meanlog)
+           +length(sdlog))/3)
+  {
 
-  print("Stop, k is incorrect length!")
+    print("Stop, k is incorrect length!")
 
-  stop()
+    stop()
+  }
+
+  parameters <- list(names=names
+                     , n=n
+                     , meanlog=meanlog
+                     , sdlog=sdlog)
+
+
+  df1 = data.frame(n , meanlog , sdlog )
+
+  df2 = apply(df1, 1, function(x) stats::rlnorm(x[1], meanlog=x[2], sdlog=x[3]))
+
+  df2 = t(purrr::map_dfr(df2, ~as.data.frame(t(.))))
+
+  df2 = as.data.frame(df2)
+
+  colnames(df2) = names
+
+  simulateddata = df2
+
+  simdata = tidyr::pivot_longer(data=df2, cols= c(1:ncol(df2)))
+
+  simdata = data.frame(simdata)
+
+
+  simdata = stats::na.omit(simdata)
+
+  P = ggplot2::ggplot(simdata, mapping = ggplot2::aes(x=.data$name,
+                                                      y=.data$value,
+                                                      color = .data$name)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::geom_jitter(alpha=0.4) +
+    ggthemes::theme_tufte() +
+    ggplot2::ggtitle("Simulated Data") +
+    ggplot2::labs(x="Group",y="Value",color="Treatment Groups")
+
+
+  print(P)
+  print(parameters)
+  print(simulateddata)
+
 }
 
-parameters <<- list(names=names
-                    , n=n
-                    , meanlog=meanlog
-                    , sdlog=sdlog)
 
 
-df1 <<- data.frame(n , meanlog , sdlog )
-
-df2 <- apply(df1, 1, function(x) stats::rlnorm(x[1], meanlog=x[2], sdlog=x[3]))
-
-df2 <- purrr::map_dfr(df2, ~as.data.frame(t(.)))
-
-df2 <- tibble::as_tibble(t(df2))
-
-df2 <<- as.data.frame(df2)
-
-colnames(df2) <- names
-
-simulateddata <<- df2
-
-simdata <- tidyr::pivot_longer(data=df2, cols= c(1:ncol(df2)))
-
-simdata <- data.frame(simdata)
-
-
-simdata <- stats::na.omit(simdata)
-
-P <- ggplot2::ggplot(simdata, mapping = aes(x=name,
-                                            y=value,
-                                            color = name)) +
-  ggplot2::geom_boxplot() +
-  ggplot2::geom_jitter(alpha=0.4) +
-  ggthemes::theme_tufte() +
-  ggplot2::ggtitle("Simulated Data") +
-  ggplot2::labs(x="Group",y="Value",color="Treatment Groups")
-
-
-print(p)
-print(parameters)
-print(simulateddata)
-
-}
 
 
